f3c243d121fc9f4d8b719f2ab793f9adfb738e9f
amulya2018 <62004103+amulya2018@users.noreply.github.com>
2020-04-24 11:12:13 +0530

    Update OtpController.java

--- 2FA/src/main/java/com/servicemarket/fa/controller/OtpController.java
+++ 2FA/src/main/java/com/servicemarket/fa/controller/OtpController.java
@@ -14,19 +14,21 @@ import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
+@RequestMapping("/fa")
+@Api(value = "Service Rest API", description = "Information about the profiles that are being provided in the organization")
 public class OtpController {
 	@ApiModelProperty(notes="autowired service for this api")
 	@Autowired
 	OtpService service;
 	@ApiOperation(value = "to send a mail", 
 			notes = "Hit this URL for getting an mail")
-	@RequestMapping(method = RequestMethod.POST, value="/fa/auth")
+	@RequestMapping(method = RequestMethod.POST, value="/auth")
 	String authenticate(@RequestBody Customer customer) {
 		return service.sendAuthMail(customer);
 	}
 	@ApiOperation(value = "getting OTP through Mail ", 
 			notes = "Hit this URL for getting an otp through mail")
-	@RequestMapping("/fa/verify/{email}/{otp_gen}")
+	@RequestMapping("/verify/{email}/{otp_gen}")
 	String validate(@PathVariable String email, @PathVariable String otp_gen) {
 		
 		return service.verifyLink(email, otp_gen);
